51a001e826a0c5e5527f14cdfeef4137a52b64b8
krishnapalsonaniya29 <krishnnapalsonaniya@gmail.com>
2026-01-23 12:10:45 +0530

    fixed settings.py

--- SAHAYAK/SAHAYAK/settings.py
+++ SAHAYAK/SAHAYAK/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('RENDER_EXTERNAL_HOSTNAME')]
+# ALLOWED_HOSTS = [os.getenv('RENDER_EXTERNAL_HOSTNAME')]
+ALLOWED_HOSTS = [
+    os.getenv("RENDER_EXTERNAL_HOSTNAME"),
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
@@ -135,7 +141,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -160,6 +171,13 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8081",
+#     "http://127.0.0.1:8081",
+#     "http://localhost:19000",
+#     "http://127.0.0.1:19000",
+#     "https://sram-thi7.onrender.com",
+# ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8081",
     "http://127.0.0.1:8081",
@@ -167,6 +185,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:19000",
     "https://sram-thi7.onrender.com",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+# TEMPORARY (OK for now, REMOVE LATER)
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 # Only for dev not for productions
