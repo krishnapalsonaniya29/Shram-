285b602cacd4ca60035db38770642beb9b3c1fb4
krishnapalsonaniya29 <krishnnapalsonaniya@gmail.com>
2026-01-23 12:13:09 +0530

    changes databse in settings.py

--- SAHAYAK/SAHAYAK/settings.py
+++ SAHAYAK/SAHAYAK/settings.py
@@ -95,6 +95,16 @@ WSGI_APPLICATION = 'SAHAYAK.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -103,8 +113,12 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
+
 
 
 # Password validation
